89f4400ec7a2fe28aa15cb6c2a0ccd518fe3e028
Siddhart21 <108049037+Siddhart21@users.noreply.github.com>
2025-04-16 20:22:31 +0530

    New Commit

--- src/components/Exp.js
+++ src/components/Exp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import food from './food.png';
 import grey from './grey.png';
 import card3 from './card3.png';
@@ -6,20 +6,24 @@ import { Link } from 'react-router-dom';
 
 const Exp = () => {
     return (
-        <div className='mt-36 font-satoshi font-base text-6xl' id='Exp'> 
+        <div className='mt-36 font-satoshi font-base text-6xl' id='Exp'>
             <p className='ml-[252px]'>Experience</p>
-            <div className='flex justify-center bg-gradient-to-r from-card via-[#b3d9a0] to-[#c3eaaf] mx-64 rounded-3xl p-7 mt-10 h-[500px]'>
-                <div className=' w-2/4 mt-2'>
+
+            {/* Fusion Center Experience */}
+            <div className='flex flex-col md:flex-row justify-center bg-gradient-to-r from-card via-[#b3d9a0] to-[#c3eaaf] mx-4 md:mx-64 rounded-3xl p-7 mt-10 h-auto md:h-[500px]'>
+                <div className='w-full md:w-2/4 mt-2'>
                     <p className='text-3xl text-left font-semibold'>Fusion Center</p>
-                    <p className='text-lg text-left mt-4 text-Text'>Enhanced Hexaware's internal tool, "Fusion Center," by redesigning its outdated UI. This tool serves as a central platform to list and access all AI solutions and tools offered by Hexaware, improving usability and efficiency</p>
-                    <Link to="/Foodview"><button className='rounded-3xl text-base bg-black  text-white pt-3 pb-3 pl-6 pr-6 hover:bg-[#65d74e] transition-all duration-200 hover:text-white mt-44'>View more</button></Link>
+                    <p className='text-lg text-left mt-4 text-Text'>Enhanced Hexaware's internal tool, "Fusion Center," by redesigning its outdated UI. This tool serves as a central platform to list and access all AI solutions and tools offered by Hexaware, improving usability and efficiency.</p>
+                    <Link to="/Foodview">
+                        <button className='rounded-3xl text-base bg-black text-white pt-3 pb-3 pl-6 pr-6 hover:bg-[#65d74e] transition-all duration-200 hover:text-white mt-10 md:mt-44'>View more</button>
+                    </Link>
                 </div>
-                <div className=' w-2/4 mt-2 px-7'>
-                    <div className='w-full h-[250px]  rounded-lg'>
-                        <img src={food} className='w-full h-full rounded-lg' alt="img" />
+                <div className='w-full md:w-2/4 mt-2 px-7'>
+                    <div className='w-full h-[250px] rounded-lg'>
+                        <img src={food} className='w-full h-full rounded-lg' alt="Food" />
                     </div>
                     <div className='mt-10 bg-gradient-to-r from-[#90d8f4] via-[#5cb1db] to-[#85cde3] rounded-lg'>
-                        <div className='text-base flex flex-row p-4 gap-4 flex-wrap'>
+                        <div className='text-base flex flex-wrap p-4 gap-4'>
                             <p className='p-3 bg-newblue text-white rounded-lg'>ReactJS</p>
                             <p className='p-3 bg-newblue text-white rounded-lg'>Tailwind</p>
                             <p className='p-3 bg-newblue text-white rounded-lg'>Router</p>
@@ -30,21 +34,22 @@ const Exp = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex justify-center mx-64 rounded-3xl p-7 mt-8 h-[500px] bg-gradient-to-r from-card2 via-[#f29696] to-[#f7b3b3]'>
-                <div className=' w-2/4 mt-2'>
+
+            {/* School Curriculum Web Application */}
+            <div className='flex flex-col md:flex-row justify-center mx-4 md:mx-64 rounded-3xl p-7 mt-8 h-auto md:h-[500px] bg-gradient-to-r from-card2 via-[#f29696] to-[#f7b3b3]'>
+                <div className='w-full md:w-2/4 mt-2'>
                     <p className='text-3xl text-left font-semibold'>School curriculum web Application</p>
-                    <p className='text-lg text-left mt-4 text-Text'>Worked on a school web application, implementing security fixes to protect against
-                        vulnerabilities and enhancing overall code quality through development of new
-                        features
-                    </p>
-                    <Link to="/Schoolview"><button className='rounded-3xl text-base bg-black  text-white pt-3 pb-3 pl-6 pr-6 hover:bg-[#f05151] transition-all duration-200 hover:text-white mt-44'>View more</button></Link>
+                    <p className='text-lg text-left mt-4 text-Text'>Worked on a school web application, implementing security fixes to protect against vulnerabilities and enhancing overall code quality through the development of new features.</p>
+                    <Link to="/Schoolview">
+                        <button className='rounded-3xl text-base bg-black text-white pt-3 pb-3 pl-6 pr-6 hover:bg-[#f05151] transition-all duration-200 hover:text-white mt-10 md:mt-44'>View more</button>
+                    </Link>
                 </div>
-                <div className=' w-2/4 mt-2 px-7'>
-                    <div className='w-full h-[250px]  rounded-lg'>
-                        <img src={grey} className='w-full h-full rounded-lg' alt="img" />
+                <div className='w-full md:w-2/4 mt-2 px-7'>
+                    <div className='w-full h-[250px] rounded-lg'>
+                        <img src={grey} className='w-full h-full rounded-lg' alt="Grey" />
                     </div>
                     <div className='mt-10 bg-gradient-to-r from-[#e1dddd] via-[#e6e3e3] to-[#b1a9a9] rounded-lg'>
-                        <div className='text-base flex flex-row p-4 gap-4 flex-wrap'>
+                        <div className='text-base flex flex-wrap p-4 gap-4'>
                             <p className='p-3 bg-black text-white rounded-lg'>ColdFusion</p>
                             <p className='p-3 bg-black text-white rounded-lg'>Javascript</p>
                             <p className='p-3 bg-black text-white rounded-lg'>Mysql</p>
@@ -55,22 +60,24 @@ const Exp = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex justify-center mx-64 rounded-3xl p-7 mt-8 h-[500px] bg-gradient-to-r from-[#e7e0eb] via-[#ccb9d7] to-[#ccbdd4]'>
-                <div className=' w-2/4 mt-2'>
+
+            {/* Portfolio Web Application */}
+            <div className='flex flex-col md:flex-row justify-center mx-4 md:mx-64 rounded-3xl p-7 mt-8 h-auto md:h-[500px] bg-gradient-to-r from-[#e7e0eb] via-[#ccb9d7] to-[#ccbdd4]'>
+                <div className='w-full md:w-2/4 mt-2'>
                     <p className='text-3xl text-left font-semibold'>Portfolio web application</p>
-                    <p className='text-lg text-left mt-4 text-Text'>I embarked on a self-initiated project to demonstrate and test my proficiency in
-                    modern web technologies. Leveraging ReactJS, React Hooks, React Router and Tailwind CSS
-                    </p>
-                    <Link to="/portview"><button className='rounded-3xl text-base bg-black  text-white pt-3 pb-3 pl-6 pr-6 hover:bg-[#eb50eb] transition-all duration-200 hover:text-white mt-52'>View more</button></Link>
+                    <p className='text-lg text-left mt-4 text-Text'>I embarked on a self-initiated project to demonstrate and test my proficiency in modern web technologies. Leveraging ReactJS, React Hooks, React Router, and Tailwind CSS.</p>
+                    <Link to="/portview">
+                        <button className='rounded-3xl text-base bg-black text-white pt-3 pb-3 pl-6 pr-6 hover:bg-[#eb50eb] transition-all duration-200 hover:text-white mt-10 md:mt-52'>View more</button>
+                    </Link>
                 </div>
-                <div className=' w-2/4 mt-2 px-7'>
-                    <div className='w-full h-[250px]  rounded-lg'>
-                        <img src={card3} className='w-full h-full rounded-lg' alt="img" />
+                <div className='w-full md:w-2/4 mt-2 px-7'>
+                    <div className='w-full h-[250px] rounded-lg'>
+                        <img src={card3} className='w-full h-full rounded-lg' alt="Card" />
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Exp
+export default Exp;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { useInView } from 'react-intersection-observer';
 
-
-
 const Skills = () => {
-    
     const { ref, inView } = useInView({
         triggerOnce: true, // Animation will only happen once
         threshold: 0.1,    // Trigger when 10% of the item is visible
-      });    
+    });
 
     return (
         <div className={`mt-36 text-center font-satoshi font-base text-6xl transition-transform duration-700 ease-out ${
             inView ? 'opacity-100 transform translate-y-0' : 'opacity-0 transform translate-y-10'
         }`} id="skills" ref={ref}>
             Skills
-            <div className='w-3/4 ml-60 mt-5 bg-blue-back px-28 py-10 rounded-3xl flex-row'>
-                <div className='flex gap-20 justify-around'>
+            <div className='w-3/4 mx-auto mt-5 bg-blue-back px-28 py-10 rounded-3xl flex-col lg:flex-row'>
+                {/* Skill Icons - Flex Layout (Desktop) and Stack Layout (Mobile) */}
+                <div className='flex flex-wrap justify-center gap-36'>
                     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" title='HTML5' rel="noopener noreferrer">
                         <img src="https://img.icons8.com/?size=100&id=D2Hi2VkJSi33&format=png&color=000000" className='w-24 h-24 bg-white rounded-full p-2 transition-transform duration-500 ease-in-out hover:scale-125 hover:rotate-180 cursor-pointer' alt="JavaScript" />
                     </a>
@@ -30,7 +28,9 @@ const Skills = () => {
                         <img src="https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000" className='w-24 h-24 bg-white rounded-full p-2 transition-transform duration-500 ease-in-out hover:scale-125 hover:rotate-180 cursor-pointer' alt="Oracle" />
                     </a>
                 </div>
-                <div className='flex gap-20 justify-around mt-12'>
+
+                {/* Space between rows without increasing container height */}
+                <div className='flex flex-wrap justify-center gap-36 mt-10'> {/* Reduced margin-top */}
                     <a href="https://www.mysql.com/" target="_blank" rel="noopener noreferrer" title='MySQL'>
                         <img src="https://img.icons8.com/?size=100&id=42904&format=png&color=000000" className='w-24 h-24 bg-white rounded-full p-2 transition-transform duration-500 ease-in-out hover:scale-125 hover:rotate-180 cursor-pointer' alt="MySQL" />
                     </a>
@@ -44,7 +44,8 @@ const Skills = () => {
                         <img src="https://img.icons8.com/?size=100&id=tGvHBPJaKqEd&format=png&color=000000" className='w-24 h-24 bg-white rounded-full p-2 transition-transform duration-500 ease-in-out hover:scale-125 hover:rotate-180 cursor-pointer' alt="Node.js" />
                     </a>
                 </div>
-                <div className='flex gap-20 justify-around mt-10'>
+
+                <div className='flex flex-wrap justify-center gap-36 mt-10'> {/* Reduced margin-top */}
                     <a href="https://jestjs.io/docs/getting-started" target="_blank" rel="noopener noreferrer" title='Jest'>
                         <img src="https://img.icons8.com/?size=100&id=bp24DwGXJDyT&format=png&color=000000" className='w-24 h-24 bg-white rounded-full p-2 transition-transform duration-500 ease-in-out hover:scale-125 hover:rotate-180 cursor-pointer' alt="HTML" />
                     </a>
@@ -55,13 +56,12 @@ const Skills = () => {
                         <img src="https://img.icons8.com/?size=100&id=21893&format=png&color=000000" className='w-24 h-24 bg-white rounded-full p-2 transition-transform duration-500 ease-in-out hover:scale-125 hover:rotate-180 cursor-pointer' alt="ColdFusion" />
                     </a>
                     <a href="https://redux.js.org/" target="_blank" rel="noopener noreferrer" title='Redux'>
-                    <img src="https://img.icons8.com/?size=100&id=b6vIINYN0kfW&format=png&color=000000" className='w-24 h-24 bg-white rounded-full p-2 transition-transform duration-500 ease-in-out hover:scale-125 hover:rotate-180 cursor-pointer' alt="ColdFusion" />
+                        <img src="https://img.icons8.com/?size=100&id=b6vIINYN0kfW&format=png&color=000000" className='w-24 h-24 bg-white rounded-full p-2 transition-transform duration-500 ease-in-out hover:scale-125 hover:rotate-180 cursor-pointer' alt="ColdFusion" />
                     </a>
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Skills
+export default Skills;
